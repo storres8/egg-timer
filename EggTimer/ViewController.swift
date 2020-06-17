f96f33fe89a0b9a473f5621d75a4385ae1b1b770
Steven Torres <stevetorres7562@gmail.com>
2020-06-16 21:52:15 -0400

    wrote conditional logic to check egg hardness

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,7 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
     
 
-
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        // titleLabel.text is mainly used to configure the text
+        // of the button for each state
+        // can also be used to modify the font
+        //print(sender.titleLabel!.text!)
+        
+        // current title is read-only is used to get the title
+        // that is currently being displayed
+        let hardness = sender.currentTitle!.lowercased()
+        print(hardness)
+        
+        if hardness == "soft" {
+            print(softTime)
+        } else if hardness == "medium" {
+            print(mediumTime)
+        } else {
+            print(hardTime)
+        }
+    }
+    
 }
