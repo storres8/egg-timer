d5580f8e71d5a1f7ba29e22ccb052375cd139611
Steven Torres <stevetorres7562@gmail.com>
2020-07-14 19:01:37 -0400

    implemented egg timer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -14,13 +14,13 @@ class ViewController: UIViewController {
     
     // egg times are saved in secconds
     let eggTimes: [String : Int] = [
-        "soft": 5,
+        "soft": 60,
         "medium": 420,
         "hard": 720
     ]
     
     var secondsRemaining:Int = 60
-    
+    var initial:Int? = nil
     var timer = Timer()
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
@@ -29,6 +29,7 @@ class ViewController: UIViewController {
         let hardness = sender.currentTitle!.lowercased()
         
         secondsRemaining = eggTimes[hardness]!
+        initial = eggTimes[hardness]!
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
         
@@ -38,6 +39,14 @@ class ViewController: UIViewController {
     @objc func updateTimer() {
         if secondsRemaining > 0 {
             print("\(secondsRemaining) seconds.")
+            
+            let numerator = Float(initial!) - Float(secondsRemaining)
+            
+            let final = numerator / Float(initial!)
+            
+            
+            progressBar.progress = final
+            
             secondsRemaining -= 1
         } else {
             timer.invalidate()
