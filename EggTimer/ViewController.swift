337ba0fd8a4d1e5cd5862d44fb36f0455b05f78a
Steven Torres <stevetorres7562@gmail.com>
2020-07-15 08:45:51 -0400

    added alarm sound when timer finishes

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     @IBOutlet weak var timerDone: UILabel!
@@ -14,7 +15,7 @@ class ViewController: UIViewController {
     
     // egg times are saved in secconds
     let eggTimes: [String : Int] = [
-        "soft": 60,
+        "soft": 5,
         "medium": 420,
         "hard": 720
     ]
@@ -22,6 +23,30 @@ class ViewController: UIViewController {
     var secondsRemaining:Int = 60
     var initial:Int? = nil
     var timer = Timer()
+    
+    var player: AVAudioPlayer?
+    
+    func playsound(){
+        guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else { return }
+
+           do {
+               try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+               try AVAudioSession.sharedInstance().setActive(true)
+
+               /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+               player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+               /* iOS 10 and earlier require the following line:
+               player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+               guard let player = player else { return }
+
+               player.play()
+
+           } catch let error {
+               print(error.localizedDescription)
+           }
+    }
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         timer.invalidate()
@@ -52,6 +77,7 @@ class ViewController: UIViewController {
             timer.invalidate()
             progressBar.progress = 1.0
             timerDone.text = "DONE!"
+            playsound()
         }
     }
 }
